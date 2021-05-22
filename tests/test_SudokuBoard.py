716c6170d0b08c902e44e32e6d4e1d061669cc4d
SchneiderDaniel <daniel.schneider.privat@googlemail.com>
2021-05-22 10:37:22 +0200

    test setup

--- tests/test_SudokuBoard.py
+++ tests/test_SudokuBoard.py
@@ -1,19 +1,14 @@
 import pytest
+from board.SudokuBoard import SudokuBoard
 
+emptyBoard = SudokuBoard(9)
 
-def capital_case(x):
-    return x.capitalize()
+def testSize():
+    assert emptyBoard.size == 9
 
-def test_capital_case():
-    assert capital_case('semaphore') == 'Semaphore'
-
-def test_raises_exception_on_non_string_arguments():
-    with pytest.raises(TypeError):
-        capital_case(9)
-
-
-
-
+def testCellAmount():
+    assert emptyBoard.cellAmount == 81
+ 
 # import pytest
 # from wallet import Wallet, InsufficientAmount
 
